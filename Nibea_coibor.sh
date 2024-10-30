630c6237a98e156f2e89538a181c3acc7585056f
Jin-Hyun Kim <kimzz1004@gmail.com>
2024-10-30 10:55:06 +0900

    Update Nibea_coibor.sh

--- Nibea_coibor.sh
+++ Nibea_coibor.sh
@@ -1,2 +1,5 @@
 https://sra-download.ncbi.nlm.nih.gov/traces/wgs04/wgs_aux/JA/LL/KU/JALLKU01/JALLKU01.1.fsa_nt.gz
 https://sra-download.ncbi.nlm.nih.gov/traces/wgs04/wgs_aux/JA/LL/KU/JALLKU01/JALLKU01.2.fsa_nt.gz
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR190/063/SRR19088063/SRR19088063_1.fastq.gz 
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR190/063/SRR19088063/SRR19088063_2.fastq.gz

cd2f1a4a6fa8cd00d93811d259425bc89cea4a53
Jin-Hyun Kim <kimzz1004@gmail.com>
2024-10-30 10:29:12 +0900

    Create Nibea_coibor.sh

---
+++ Nibea_coibor.sh
@@ -0,0 +1,2 @@
+https://sra-download.ncbi.nlm.nih.gov/traces/wgs04/wgs_aux/JA/LL/KU/JALLKU01/JALLKU01.1.fsa_nt.gz
+https://sra-download.ncbi.nlm.nih.gov/traces/wgs04/wgs_aux/JA/LL/KU/JALLKU01/JALLKU01.2.fsa_nt.gz
